3d2742c28d6e12f0a8f3af185e18887baca14e63
Gaurav Gupta <itisgauravgupta@gmail.com>
2024-12-10 15:21:41 +0530

    config

---
+++ src/dbconfig/server/dbSetup.ts
@@ -0,0 +1,33 @@
+import { db_name } from "@/constants";
+import createQuestionCollection from "@/models/question.collection";
+import createAnswerCollection from "@/models/answer.collection";
+import createCommentCollection from "@/models/comment.collection";
+import createVoteCollection from "@/models/vote.collection";
+import createStorage from "@/models/storage.setup";
+
+import { db } from "@/dbconfig/server/config";
+
+export default async function dbSetup() {
+  try {
+    await db.get(db_name);
+    console.log("Database connection");
+  } catch (error) {
+    try {
+      await db.create(db_name, db_name);
+      console.log("database created");
+      //create collections
+      await Promise.all([
+        createQuestionCollection(),
+        createAnswerCollection(),
+        createCommentCollection(),
+        createVoteCollection(),
+      ]);
+      console.log("Collection created");
+      console.log("Database connected");
+    } catch (error) {
+      console.log("Error creating databases or collection", error);
+    }
+  }
+
+  return db;
+}
